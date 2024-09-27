9f9bcd0e3909d9283b7391436213825feeb6fbd6
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-28 01:52:26 +0530

    Add public-samples/apt-finder-v2-oybclj/src/api/interfaces/index.ts

---
+++ src/api/interfaces/index.ts
@@ -0,0 +1,15 @@
+// src/api/interfaces/index.ts
+
+// Import interfaces
+import { RequestInterface } from './request.interface';
+import { ResponseInterface } from './response.interface';
+
+// Export interfaces
+export {
+    RequestInterface,
+    ResponseInterface
+};
+
+// Human tasks:
+// TODO: Create and implement the RequestInterface in request.interface.ts
+// TODO: Create and implement the ResponseInterface in response.interface.ts
