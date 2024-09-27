d697ce86d4f5cf669038cb763a8294197114e778
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-28 01:57:06 +0530

    Add public-samples/apt-finder-v2-oybclj/src/shared/assets/icons/index.ts

---
+++ src/shared/assets/icons/index.ts
@@ -0,0 +1,23 @@
+// src/shared/assets/icons/index.ts
+
+// Import SVG icons
+import { ReactComponent as HomeIcon } from './HomeIcon.svg';
+import { ReactComponent as SearchIcon } from './SearchIcon.svg';
+import { ReactComponent as FilterIcon } from './FilterIcon.svg';
+import { ReactComponent as UserIcon } from './UserIcon.svg';
+import { ReactComponent as SubscriptionIcon } from './SubscriptionIcon.svg';
+import { ReactComponent as LogoutIcon } from './LogoutIcon.svg';
+
+// Export icon components
+export {
+  HomeIcon,
+  SearchIcon,
+  FilterIcon,
+  UserIcon,
+  SubscriptionIcon,
+  LogoutIcon,
+};
+
+// Human tasks:
+// TODO: Create individual SVG icon files for each icon (HomeIcon.svg, SearchIcon.svg, etc.)
+// TODO: Optimize SVG files for web use
