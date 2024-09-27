3318334eaf306f8eee43e6c5de98fceb5b118f3e
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-28 01:52:03 +0530

    Add public-samples/apt-finder-v2-oybclj/infrastructure/terraform/modules/vpc/outputs.tf

---
+++ infrastructure/terraform/modules/vpc/outputs.tf
@@ -0,0 +1,33 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "List of IDs of public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of IDs of private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "nat_gateway_ids" {
+  description = "List of IDs of NAT Gateways"
+  value       = aws_nat_gateway.main[*].id
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.main.id
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+# Human tasks:
+# TODO: Review and adjust the outputs based on the specific VPC configuration and requirements of the Apartment Finder web service
+# TODO: Ensure that these outputs align with the inputs expected by other modules that depend on the VPC
