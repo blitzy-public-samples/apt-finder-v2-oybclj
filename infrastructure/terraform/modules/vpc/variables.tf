113d9ca6d9d2d3afd769c58fd54ee4fd00ef3dbf
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-28 01:52:04 +0530

    Add public-samples/apt-finder-v2-oybclj/infrastructure/terraform/modules/vpc/variables.tf

---
+++ infrastructure/terraform/modules/vpc/variables.tf
@@ -0,0 +1,62 @@
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+  default     = "apartment-finder-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "azs" {
+  description = "A list of availability zones in the region"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
+
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+variable "enable_nat_gateway" {
+  description = "Should be true if you want to provision NAT Gateways for each of your private networks"
+  type        = bool
+  default     = true
+}
+
+variable "single_nat_gateway" {
+  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
+  type        = bool
+  default     = false
+}
+
+variable "enable_vpn_gateway" {
+  description = "Should be true if you want to create a new VPN Gateway resource and attach it to the VPC"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default = {
+    Project     = "ApartmentFinder"
+    Environment = "Production"
+    ManagedBy   = "Terraform"
+  }
+}
+
+# Human tasks (commented)
+# TODO: Review and adjust the default values for VPC CIDR, subnets, and availability zones based on the specific deployment requirements and region
+# TODO: Confirm if VPN gateway is needed for this infrastructure and adjust the default value accordingly
+# TODO: Verify if the proposed subnet division between private and public subnets is appropriate for the Apartment Finder web service architecture
