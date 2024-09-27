b625579963b9f80a59f08cfbfd64a14ed7e0ebc7
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-28 01:53:31 +0530

    Add public-samples/apt-finder-v2-oybclj/src/backend/apartment_finder/wsgi.py

---
+++ src/backend/apartment_finder/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for the Apartment Finder project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+# Set the Django settings module for the WSGI application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apartment_finder.settings')
+
+# Get the WSGI application
+application = get_wsgi_application()
