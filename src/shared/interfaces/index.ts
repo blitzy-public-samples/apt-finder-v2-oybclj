33ed4d4e56fe6590cda4a0918807f4ef28d7f8bc
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-28 01:57:25 +0530

    Add public-samples/apt-finder-v2-oybclj/src/shared/interfaces/index.ts

---
+++ src/shared/interfaces/index.ts
@@ -0,0 +1,21 @@
+// src/shared/interfaces/index.ts
+
+// Import interfaces from their respective files
+import { UserInterface } from './user.interface';
+import { ApartmentInterface } from './apartment.interface';
+import { FilterInterface } from './filter.interface';
+import { SubscriptionInterface } from './subscription.interface';
+
+// Re-export interfaces
+export {
+  UserInterface,
+  ApartmentInterface,
+  FilterInterface,
+  SubscriptionInterface
+};
+
+// Human tasks:
+// TODO: Implement the UserInterface in user.interface.ts
+// TODO: Implement the ApartmentInterface in apartment.interface.ts
+// TODO: Implement the FilterInterface in filter.interface.ts
+// TODO: Implement the SubscriptionInterface in subscription.interface.ts
